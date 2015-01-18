4f3a247048bf8db1d57ff8ec687bfb0876a1c512
Geetha <geethajcpura@gmail.com>
2015-01-18 23:37:21 +0530

    Status Message Update

---
+++ Phylogenetic Tree/View Controllers/FASTA Extractors/PHFileChoserProtocols.h
@@ -0,0 +1,13 @@
+//
+//  PHFileChoserProtocols.h
+//  Phylogenetic Tree
+//
+//  Created by Sid on 18/01/15.
+//  Copyright (c) 2015 Adroit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol PHFileChoserProtocols <NSObject>
+
+@end
