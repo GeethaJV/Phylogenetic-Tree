b47f8ce36857a157210a4900ccca04406076c429
Geetha <geethajcpura@gmail.com>
2015-01-19 22:15:13 +0530

    Adding Realm Support

---
+++ Phylogenetic Tree/Database/Allignment.h
@@ -0,0 +1,17 @@
+//
+//  Allignment.h
+//  Phylogenetic Tree
+//
+//  Created by Sid on 19/01/15.
+//  Copyright (c) 2015 Adroit. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface Allignment : RLMObject
+<# Add properties here to define the model #>
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<Allignment>
+RLM_ARRAY_TYPE(Allignment)
---
+++ Phylogenetic Tree/Database/FASTAFile.h
@@ -0,0 +1,17 @@
+//
+//  FASTAFile.h
+//  Phylogenetic Tree
+//
+//  Created by Sid on 19/01/15.
+//  Copyright (c) 2015 Adroit. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface FASTAFile : RLMObject
+<# Add properties here to define the model #>
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<FASTAFile>
+RLM_ARRAY_TYPE(FASTAFile)
